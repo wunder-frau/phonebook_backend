3f50f5386d0351ae25f144aa046eb35fb5357861
irene <wunder.frau@yandex.by>
2025-01-20 23:51:37 +0200

    part_03: 3.3: Phonebook backend step 3

--- index.js
+++ index.js
@@ -40,6 +40,16 @@ app.get("/info", (request, response) => {
   response.send(info);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
