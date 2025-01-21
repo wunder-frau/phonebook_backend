9c4281be2dbae7bbb2e27aad962699822068ae4f
irene <wunder.frau@yandex.by>
2025-01-21 12:53:10 +0200

    part_03: 3.4: Phonebook backend step 5/  HTTP POST requests

--- index.js
+++ index.js
@@ -62,6 +62,22 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Name or number is missing",
+    });
+  }
+  const person = {
+    id: String(persons.length + 1),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
