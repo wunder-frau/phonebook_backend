3e0a8adcf1cf8767c9becea60be75ae319c75906
irene <wunder.frau@yandex.by>
2025-01-23 00:12:59 +0200

    part_03: Exercises 3.7.-3.8.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 let persons = [
@@ -31,6 +32,11 @@ const generateId = () => {
 };
 
 app.use(express.json());
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
